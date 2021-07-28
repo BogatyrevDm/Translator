1f1ab189e87ed03b0f29f566b76aff9efa3927c3
Dmitrii Bogatyrev <bogatyrevdm82@gmail.com>
2021-07-28 08:50:48 +0300

    task done

---
+++ app/src/main/java/com/example/translator/model/data/Meanings.kt
@@ -0,0 +1,9 @@
+package com.example.translator.model.data
+
+import com.google.gson.annotations.SerializedName
+
+class Meanings(
+    @SerializedName("translation") val translation: Translation?,
+    @SerializedName("imageUrl") val imaggeUrl: String?,
+    @SerializedName("transcription") val transcription: String?
+)
